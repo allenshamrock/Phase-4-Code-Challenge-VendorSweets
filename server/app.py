da1a3b8c5f8e06c9e11e69f728b3d644338e5fa0
Allen <allenshamrock37@gmail.com>
2024-04-15 19:12:11 +0300

    {Feat}:Implemented DELETE & GET methods

--- server/app.py
+++ server/app.py
@@ -18,6 +18,7 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 @app.route('/')
 def home():
@@ -34,12 +35,66 @@ class Vendors(Resource):
 class VendorsById(Resource):
     def get(self,id):
         vendor = Vendor.query.filter(Vendor.id == id).first()
-        if vendor:
-            response  = make_response(vendor.to_dict(),200)
+        if not vendor:
+            response = make_response({"error": "Vendor not found"}, 404)
             return response
-        response = make_response({"error": "Vendor not found"}, 404)
+        response = make_response(vendor.to_dict(), 200)
         return response
-    
+
+
+class Sweets(Resource):
+    def get(self):
+        sweets = [sweet.to_dict() for sweet in Sweet.query.all()]
+        response = make_response(sweets, 200)
+        return response
+
+
+class SweetsById(Resource):
+    def get(self, id):
+        sweet = Sweet.query.filter(Sweet.id == id).first()
+        if not sweet:
+            response = make_response({"error": "Sweet not found"}, 404)
+            return  response
+        response = make_response(sweet.to_dict(), 200)
+        return response
+
+
+class VendorSweets(Resource):
+    def post(self):
+        data = request.get_json()
+
+        price = data.get('price')
+        vendor_id = data.get('vendor_id')
+        sweet_id = data.get('sweet_id')
+
+        if price is None or vendor_id is None or sweet_id is None:
+            return {"errors": ["validation errors"]}, 404
+
+        vendor_sweet = VendorSweet(
+            price=price, vendor_id=vendor_id, sweet_id=sweet_id)
+
+        db.session.add(vendor_sweet)
+        db.session.commit()
+        return vendor_sweet.to_dict(), 201
+
+
+class VendorSweetsyId(Resource):
+    def delete(self, id):
+        vendors_sweet = VendorSweet.query.filter_by(id=id).first()
+        if not vendors_sweet:
+            return {"error": "VendorSweet not found"}, 400
+        db.session.delete(vendors_sweet)
+        db.session.commit()
+        return {}, 204
+
+
+
+api.add_resource(Vendors, "/vendors")
+api.add_resource(Sweets, "/sweets")
+api.add_resource(VendorSweets, "/vendor_sweets")
+api.add_resource(VendorsById, "/vendors/<int:id>")
+api.add_resource(SweetsById, "/sweets/<int:id>")
+api.add_resource(VendorSweetsyId, "/ vendor_sweets/<int: id >")
 
         
 
